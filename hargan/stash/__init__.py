2dc89c27d8e1029a6dbd47a29614f89e1b3ce3ec
Nhat-Tan Duong <vinthian@gmail.com>
2013-11-19 19:03:50 -0500

    PathSessions are now threaded.

--- hargan/stash/__init__.py
+++ hargan/stash/__init__.py
@@ -1,5 +1,63 @@
 import wx
 from poe.web_api.session import PathSession, NotLoggedInException
+from threading import *
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+ID_LOGIN_BUTTON = wx.NewId()
+ID_RESULT_LOGIN = wx.NewId()
+
+
+def EVENT_RESULT(window, result_id, func):
+    window.Connect(-1, -1, result_id, func)
+
+
+class ResultEvent(wx.PyEvent):
+    def __init__(self, data):
+        wx.PyEvent.__init__(self)
+        self.SetEventType(ID_RESULT_LOGIN)
+        self.data = data
+        logger.debug("Event data: %s" % data)
+
+
+class SessionThread(Thread):
+    def __init__(self, notify_window, username, password):
+        Thread.__init__(self)
+        self._notify_window = notify_window
+        self.username = username
+        self.password = password
+        self.start()
+
+    def run(self):
+        wx.PostEvent(self._notify_window, ResultEvent({
+            "event": "logging_in",
+            "status": "Logging in...",
+        }))
+        self.session = PathSession()
+
+        self.session.login(self.username, self.password)
+
+        if not self.session.logged_in:
+            wx.PostEvent(self._notify_window, ResultEvent({
+                "event": "login_failed",
+                "status": "Couldn't log in",
+            }))
+
+            return False
+
+        wx.PostEvent(self._notify_window, ResultEvent({
+            "event": "get_stash",
+            "status": "Getting stash tabs...",
+        }))
+
+        self.session.get_stash(league="nemesis")
+
+        wx.PostEvent(self._notify_window, ResultEvent({
+            "event": "done",
+            "status": "Logged in",
+        }))
 
 
 class StashTab(object):
@@ -17,7 +75,9 @@ class LoginUI(wx.Frame):
         super(LoginUI, self).__init__(size=(300, 150), *args, **kwargs)
         self.InitUI()
         self.Centre()
+        self.Bind(wx.EVT_CLOSE, self.close)
 
+        self.worker = None
         self.main_window = main_window
 
     def InitUI(self):
@@ -30,18 +90,20 @@ class LoginUI(wx.Frame):
         self.username = wx.TextCtrl(panel)
         self.password = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
 
+        login_button = wx.Button(panel, ID_LOGIN_BUTTON, "Login")
+        self.Bind(wx.EVT_BUTTON, self.login, id=ID_LOGIN_BUTTON)
+        EVENT_RESULT(self, ID_RESULT_LOGIN, self.result)
+
+        self.status = wx.StaticText(panel, label="")
+
         grid.AddMany([
             wx.StaticText(panel, label="Email"),
             (self.username, 1, wx.EXPAND),
             wx.StaticText(panel, label="Password"),
             (self.password, 1, wx.EXPAND),
+            (login_button, 1, wx.EXPAND),
+            self.status,
         ])
-
-        login_button = wx.Button(panel, label="Login")
-        login_button.Bind(wx.EVT_BUTTON, self.login)
-
-        # TODO: Figure out how to make this span 2 columns
-        grid.Add(login_button, 1, wx.EXPAND)
 
         grid.AddGrowableRow(2, 1)
         grid.AddGrowableCol(1, 1)
@@ -51,11 +113,23 @@ class LoginUI(wx.Frame):
         panel.SetSizer(vbox)
 
     def login(self, event):
-        if self.main_window.login(self.username.GetValue(),
-                                  self.password.GetValue()):
+        if not self.worker:
+            self.worker = SessionThread(self,
+                                        self.username.GetValue(),
+                                        self.password.GetValue())
+
+    def result(self, event):
+        self.status.SetLabel(event.data["status"])
+
+        if event.data["event"] == "done":
             self.main_window.Show()
             self.Destroy()
-        self.Show()
+
+        self.worker = None
+
+    def close(self, event):
+        self.main_window.Destroy()
+        self.Destroy()
 
 
 class MainUI(wx.Frame):
@@ -87,19 +161,6 @@ class MainUI(wx.Frame):
         self.login_window = LoginUI(self, None, title="Login")
         self.login_window.Show()
 
-    def login(self, username, password):
-        # TODO: Should move out of here and use something to thread
-        self.session = PathSession()
-        self.session.login(username, password)
-
-        if not self.session.logged_in:
-            self.login_window.Show()
-            self.Hide()
-            return False
-
-        self.session.get_stash(league="nemesis")
-
-        return True
 
 if __name__ == "__main__":
     app = wx.App(redirect=False)
