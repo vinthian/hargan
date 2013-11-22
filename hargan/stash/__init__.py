d69dcd69b56313ddc75da1af64f1532afb814c4e
Nhat-Tan Duong <vinthian@gmail.com>
2013-11-21 23:17:06 -0500

    Reorganized login window layout. Added UI to login with PHPSESSID cookies.

--- hargan/stash/__init__.py
+++ hargan/stash/__init__.py
@@ -1,5 +1,9 @@
 import wx
-from poe.web_api.session import PathSession, NotLoggedInException
+from poe.web_api.session import (
+    PathSession, InvalidLoginException, InvalidSessionIDException
+    )
+# TODO: Remove this later
+from poe.config import settings
 from threading import *
 import logging
 
@@ -7,6 +11,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 ID_LOGIN_BUTTON = wx.NewId()
+ID_SWITCH_BUTTON = wx.NewId()
 ID_RESULT_LOGIN = wx.NewId()
 
 
@@ -23,28 +28,30 @@ class ResultEvent(wx.PyEvent):
 
 
 class SessionThread(Thread):
-    def __init__(self, notify_window, username, password):
+    def __init__(self, notify_window, use_session_id, credentials):
         Thread.__init__(self)
         self._notify_window = notify_window
-        self.username = username
-        self.password = password
+        self.use_session_id = use_session_id
+        self.credentials = credentials
         self.start()
 
     def run(self):
         wx.PostEvent(self._notify_window, ResultEvent({
             "event": "logging_in",
-            "status": "Logging in...",
+            "status": "Logging in with credentials: %s..." % self.credentials[0],
         }))
         self.session = PathSession()
 
-        self.session.login(self.username, self.password)
-
-        if not self.session.logged_in:
+        try:
+            if self.use_session_id:
+                self.session.login_session_id(self.credentials[0])
+            else:
+                self.session.login_normal(self.credentials[0], self.credentials[1])
+        except (InvalidLoginException, InvalidSessionIDException) as e:
             wx.PostEvent(self._notify_window, ResultEvent({
-                "event": "login_failed",
-                "status": "Couldn't log in",
+                "event": "login_exception",
+                "status": e.args[1],
             }))
-
             return False
 
         wx.PostEvent(self._notify_window, ResultEvent({
@@ -72,7 +79,8 @@ class Stash(object):
 
 class LoginUI(wx.Frame):
     def __init__(self, main_window, *args, **kwargs):
-        super(LoginUI, self).__init__(size=(300, 150), *args, **kwargs)
+        super(LoginUI, self).__init__(size=(300, 175), *args, **kwargs)
+        self.use_session_id = False
         self.InitUI()
         self.Centre()
         self.Bind(wx.EVT_CLOSE, self.close)
@@ -83,53 +91,97 @@ class LoginUI(wx.Frame):
     def InitUI(self):
         panel = wx.Panel(self)
 
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        username_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        password_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # options_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        buttons_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        grid = wx.FlexGridSizer(2, 2, 9, 25)
+        # TODO: Remove settings later
+        self.username_text = wx.StaticText(panel, label="Email")
+        self.username = wx.TextCtrl(panel, value=settings["USERNAME"])
 
-        self.username = wx.TextCtrl(panel)
-        self.password = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+        username_sizer.Add(self.username_text, proportion=1, flag=wx.CENTER)
+        username_sizer.Add(self.username, proportion=3, flag=wx.CENTER)
 
-        login_button = wx.Button(panel, ID_LOGIN_BUTTON, "Login")
+        self.password = wx.TextCtrl(panel, value=settings["PASSWORD"],
+                                    style=wx.TE_PASSWORD)
+
+        self.password_text = wx.StaticText(panel, label="Password")
+        password_sizer.Add(self.password_text, proportion=1, flag=wx.CENTER)
+        password_sizer.Add(self.password, proportion=3, flag=wx.CENTER)
+
+        # TODO: options_sizer
+
+        self.login_button = wx.Button(panel, ID_LOGIN_BUTTON, "Login")
         self.Bind(wx.EVT_BUTTON, self.login, id=ID_LOGIN_BUTTON)
         EVENT_RESULT(self, ID_RESULT_LOGIN, self.result)
+        self.login_button.SetFocus()
 
-        self.status = wx.StaticText(panel, label="")
+        self.switch_login_type_button = wx.Button(panel, ID_SWITCH_BUTTON,
+                                                  label="Switch Login Type")
+        self.Bind(wx.EVT_BUTTON, self.switch_login_type, id=ID_SWITCH_BUTTON)
 
-        grid.AddMany([
-            wx.StaticText(panel, label="Email"),
-            (self.username, 1, wx.EXPAND),
-            wx.StaticText(panel, label="Password"),
-            (self.password, 1, wx.EXPAND),
-            (login_button, 1, wx.EXPAND),
-            self.status,
+        buttons_sizer.Add(self.login_button, proportion=1,
+                          flag=wx.ALL | wx.EXPAND)
+        buttons_sizer.Add(self.switch_login_type_button, proportion=2,
+                          flag=wx.ALL | wx.EXPAND)
+
+        main_sizer.AddMany([
+            (username_sizer, 1, wx.ALL | wx.EXPAND, 20),
+            (password_sizer, 1, wx.ALL | wx.EXPAND, 20),
+            # (options_sizer, 1, wx.ALL | wx.EXPAND, 20),
+            (buttons_sizer, 2, wx.ALL | wx.EXPAND, 15),
         ])
 
-        grid.AddGrowableRow(2, 1)
-        grid.AddGrowableCol(1, 1)
+        panel.status = self.CreateStatusBar()
+        self.status = panel.status
 
-        vbox.Add(grid, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
+        self.SetMinSize(self.GetSize())
+        self.SetMaxSize(self.GetSize())
+        self.Centre()
 
-        panel.SetSizer(vbox)
+        self.SetSizer(main_sizer)
 
     def login(self, event):
         if not self.worker:
-            self.worker = SessionThread(self,
-                                        self.username.GetValue(),
-                                        self.password.GetValue())
+            self.login_button.Disable()
+            self.switch_login_type_button.Disable()
+            if self.use_session_id:
+                credentials = (self.username.GetValue(),)
+            else:
+                credentials = (self.username.GetValue(), self.password.GetValue())
+            self.worker = SessionThread(self, self.use_session_id, credentials)
+        event.Skip()
 
     def result(self, event):
-        self.status.SetLabel(event.data["status"])
+        self.status.SetStatusText(event.data["status"])
 
         if event.data["event"] == "done":
             self.main_window.Show()
             self.Destroy()
+        if event.data["event"] == "login_exception":
+            self.login_button.Enable()
+            self.switch_login_type_button.Enable()
 
         self.worker = None
 
     def close(self, event):
         self.main_window.Destroy()
         self.Destroy()
+        event.Skip()
+
+    def switch_login_type(self, event):
+        self.use_session_id = not self.use_session_id
+        if self.use_session_id:
+            self.username_text.SetLabel("Session ID")
+            self.password_text.Hide()
+            self.password.Hide()
+        else:
+            self.username_text.SetLabel("Email")
+            self.password_text.Show()
+            self.password.Show()
+        event.Skip()
 
 
 class MainUI(wx.Frame):
